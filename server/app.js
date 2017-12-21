2ec16d5431c7a32c7fccf15a666a9af79f1b85a0
Jade <jade@meteor.com>
2017-12-20 23:46:32 -0800

    comments

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ var sendSMS = function(){
      if (!err) { // "err" is an error received during the request, if any
       // "responseData" is a JavaScript object containing data received from Twilio.        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
       // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-      console.log(responseData.from); // outputs "+18318884815"
+      console.log(responseData.from); // outputs the 'from' number
       console.log(responseData.body); // outputs "Doorbell rang!"
     }
   });

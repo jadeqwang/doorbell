7b122b8e224a3d0d8cf2e40b328a5af79f5ea44d
Jade <jade@meteor.com>
2017-12-20 23:00:02 -0800

    sends SMS to me upon button press

--- server/app.js
+++ server/app.js
@@ -6,6 +6,22 @@ var serialPort = new SerialPort.SerialPort('/dev/tty.usbmodem1421', {
   parser: SerialPort.parsers.readline('\r\n')
 });
 
+var sendSMS = function(){
+  twilio = Twilio(ACCOUNT_SID, AUTH_TOKEN);
+  twilio.sendSms({
+    to:'+18478349806', // Any number Twilio can deliver to
+    from: '+18318884815', // A number you bought from Twilio and can use for outbound communication
+    body: 'Doorbell rang!' // body of the SMS message
+  }, function(err, responseData) { //this function is executed when a response is received from Twilio
+     if (!err) { // "err" is an error received during the request, if any
+      // "responseData" is a JavaScript object containing data received from Twilio.        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+      // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+      console.log(responseData.from); // outputs "+18318884815"
+      console.log(responseData.body); // outputs "Doorbell rang!"
+    }
+  });
+}
+
 Meteor.startup(function() {
   Lights.remove({});
   Lights.insert({
@@ -39,6 +55,7 @@ serialPort.on('data', Meteor.bindEnvironment(function(data) {
   if (parsedData.messageType === 'buttonPress') {
     if (parsedData.state === 1) {
       console.log('Button Pressed!');
+      sendSMS();
     };
   }
   

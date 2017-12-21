e6605eb2852674a317ea3397b7cad4339dc4d9a5
Jade <jade@meteor.com>
2017-12-20 22:22:45 -0800

    delete more code

--- client/app.js
+++ client/app.js
@@ -14,6 +14,7 @@ Template.main.helpers({
     return this.messageType === messageType;
   }
 });
+
 Template.main.events({
   'click #startDoorbell': function(e, t) {
     e.preventDefault();
@@ -22,26 +23,5 @@ Template.main.events({
 });
 
 Template.main.onCreated(function() {
-
-  var self = this;
-  var defaultLight = Lights.find({
-    pin: 13
-  });
-  self.subscribe('lights');
-  self.defaultLight = function() {
-    return defaultLight && defaultLight.fetch()[0];
-  }
-
-  defaultLight.observe({
-    changed: function(newDoc, oldDoc) {
-      var tmpDoc = newDoc;
-      tmpDoc.created = new Date();
-      tmpDoc.messageType = 'pinChange';
-      delete tmpDoc._id;
-      Meteor.call('message', tmpDoc);
-    }
-  });
-  self.subscribe('messages');
-  
 
 });

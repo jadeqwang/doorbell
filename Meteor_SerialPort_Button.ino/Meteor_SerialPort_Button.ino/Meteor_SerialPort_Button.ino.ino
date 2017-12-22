9b57363d774b61faefa36641217bda82d08a7622
Jade <jade@meteor.com>
2017-12-21 21:38:08 -0800

    debounce the button
    now it won’t send me 3-4 texts in a row if I don’t press it exactly
    right

--- Meteor_SerialPort_Button.ino/Meteor_SerialPort_Button.ino/Meteor_SerialPort_Button.ino.ino
+++ Meteor_SerialPort_Button.ino/Meteor_SerialPort_Button.ino/Meteor_SerialPort_Button.ino.ino
@@ -3,12 +3,13 @@ const int ledPin = 13;       // number of LED pin
 
 int buttonState = 0;         // variable for storing the pushbutton status
 int newButtonState = 0;      // variable for reading the pushbutton status
+int debounceDelay = 500;    // the debounce time; increase if the output flickers
 
 void setup() {               // this runs only once upon device start
   pinMode(ledPin, OUTPUT);   // initialize the LED pin as an output
   Serial.begin(9600);        // initialize Serial port
   pinMode(buttonPin, INPUT); // initialize the pushbutton pin as an input
-  buttonState = digitalRead(buttonPin); // read button state the first time
+  buttonState = digitalRead(buttonPin);       // read button state the first time
 }
 
 void sendData() {            // crafts some JSON to send to the server
@@ -24,13 +25,15 @@ void sendData() {            // crafts some JSON to send to the server
 }
 
 void loop() {                // this loops continuously while device is on  
-  newButtonState = digitalRead(buttonPin); // read new Button state
-  digitalWrite(ledPin, newButtonState);    // LED is on if the button is on, vice versa
+  newButtonState = digitalRead(buttonPin);   // read new Button state
+  digitalWrite(ledPin, newButtonState);      // LED is on if the button is on, vice versa
+  // reading = digitalRead(buttonPin);
  
-  if (newButtonState == HIGH && buttonState == LOW && Serial.available() > 0) { 
-    // if the button just got pressed AND the Serial connection is available
-      sendData(); // send the button press to the server
+  if (newButtonState == HIGH && buttonState == LOW) { // if button just got pressed
+      if (Serial.available() > 0) {                   // and Serial connection is available
+        sendData();                                   // send the button press to the server
+        delay(debounceDelay);                         // wait a moment (debounce)
+    }
   }
-  
-  buttonState = newButtonState;            // store the button state for comparison
+  buttonState = newButtonState;              // store the button state for comparison
 }
